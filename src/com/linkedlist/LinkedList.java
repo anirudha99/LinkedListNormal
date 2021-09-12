38ee9c6ececb8081f5ee60678a46112de95583da
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-12 11:17:35 +0530

    Use case 1

---
+++ src/com/linkedlist/LinkedList.java
@@ -0,0 +1,66 @@
+package com.linkedlist;
+
+public class LinkedList {
+	private Node head;
+
+	private class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+
+	/**
+	 * method to insert node at the end
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public boolean addEnd(final int data) {
+		boolean isAdded = false;
+
+		Node newNode = new Node(data);
+
+		if (head == null) {
+			head = newNode;
+			isAdded = true;
+
+		} else {
+			Node temp = head;
+
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+			isAdded = true;
+		}
+		return isAdded;
+	}
+	/*
+	 * method to print nodes in linked list
+	 */
+
+	public void print() {
+		Node temp = head;
+		System.out.println("Linkedlist ");
+		while (temp.next != null) {
+			System.out.print(temp.data + "-->");
+			temp = temp.next;
+		}
+		System.out.print(temp.data + "-->null");
+	}
+
+	/**
+	 * method to node at head or insert node front
+	 * @param data
+	 */
+	public void addFront(int data) {
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+	}
+
+}
---
+++ src/com/linkedlist/LinkedlistMain.java
@@ -0,0 +1,13 @@
+package com.linkedlist;
+
+public class LinkedlistMain {
+
+	public static void main(String[] args) {
+		LinkedList l = new LinkedList();
+		l.addFront(100);
+		l.addFront(200);
+		l.addFront(300);
+		l.addFront(400);
+		l.print();
+	}
+}
