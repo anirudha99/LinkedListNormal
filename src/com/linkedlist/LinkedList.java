c751530dc2024c7c17e4cb71a7b2f54480ecacc7
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-15 08:50:43 +0530

    Added sorted & ordered linked list

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -88,6 +88,7 @@ public class LinkedList {
 			}
 			newNode.next = cur;
 			prev.next = newNode;
+			
 		}
 
 	}
--- src/com/linkedlist/LinkedlistMain.java
+++ src/com/linkedlist/LinkedlistMain.java
@@ -3,21 +3,30 @@ package com.linkedlist;
 public class LinkedlistMain {
 
 	public static void main(String[] args) {
-		LinkedList l = new LinkedList();
-		l.addFront(100);
-		l.addFront(200);
-		l.addFront(300);
-		l.addFront(400);
-		l.print();
+		
+//		LinkedList l = new LinkedList();
+//		l.addFront(100);
+//		l.addFront(200);
+//		l.addFront(300);
+//		l.addFront(400);
+//		l.print();
 //		l.deleteAtHead();
 //		l.print();
 //		l.deleteAtTail();
 //		l.print();
-		l.search(400);
-		l.addAtPosition(3, 250);
-		l.insertAfter(400, 450);
-		l.print();
-		l.deleteSpecified(450);
-		l.print();
+//		l.search(400);
+//		l.addAtPosition(3, 250);
+//		l.insertAfter(400, 450);
+//		l.print();
+//		l.deleteSpecified(450);
+//		l.print();
+		
+		OrderedLinkedList orderedList = new OrderedLinkedList();
+		orderedList.add(56);
+		orderedList.add(30);
+		orderedList.add(40);
+		orderedList.add(72);
+		System.out.println("Ordered list is");
+		orderedList.print();
 	}
 }
